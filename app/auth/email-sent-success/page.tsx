295503e4afba502ea62010ad1568daf07a74399d
Ekbal41 <mdasifekbalshagor@gmail.com>
2024-01-09 17:12:52 +0600

    ok

--- app/auth/email-sent-success/page.tsx
+++ app/auth/email-sent-success/page.tsx
@@ -1,7 +1,6 @@
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
-
 export default function RecoveryEmailSent() {
   return (
     <div className="max-w-md mx-auto my-10">
@@ -14,12 +13,12 @@ export default function RecoveryEmailSent() {
           height={200}
         />
       </div> */}
-      <div className="text-center mt-4">
-        <h1 className="my-3 text-2xl lg:text-3xl font-semibold ">
+      <div className="flex justify-center flex-col mt-4 ">
+        <h1 className="my-3 text-2xl lg:text-3xl font-semibold text-center">
           Email Sent Successfully
         </h1>
-        <p className="text-gray-500 ">
-          Please check your email and follow the instructions. follow the instructions.
+        <p className="text-gray-500 w-80 mx-auto text-center">
+          Please check your inbox and follow the given instructions. Thank you!
         </p>
       </div>
     </div>
