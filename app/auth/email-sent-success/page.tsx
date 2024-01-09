abb6d953812b6dbeec884fe061e75c1357f81745
Ekbal41 <mdasifekbalshagor@gmail.com>
2024-01-09 17:15:54 +0600

    done

--- app/auth/email-sent-success/page.tsx
+++ app/auth/email-sent-success/page.tsx
@@ -4,15 +4,7 @@ import Image from "next/image";
 export default function RecoveryEmailSent() {
   return (
     <div className="max-w-md mx-auto my-10">
-      <EnvelopeIcon className="mx-auto h-12 w-12 text-gray-400" />
-      {/* <div className="flex justify-center">
-        <Image
-          src="/email-sent.svg"
-          alt="Email Sent"
-          width={200}
-          height={200}
-        />
-      </div> */}
+      <EnvelopeIcon className="mx-auto h-14 w-14 text-gray-400" />
       <div className="flex justify-center flex-col mt-4 ">
         <h1 className="my-3 text-2xl lg:text-3xl font-semibold text-center">
           Email Sent Successfully
