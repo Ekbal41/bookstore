35328e92fe824202776d41091a8a54911aafbd2f
Ekbal41 <mdasifekbalshagor@gmail.com>
2024-01-14 15:38:03 +0600

    ok

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,174 @@
+"use client";
+import MainPage from "@/components/dashboard/MainPage";
+import { Tab } from "@headlessui/react";
+import {
+  BanknotesIcon,
+  HomeIcon,
+  RectangleStackIcon,
+  ShieldCheckIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
+export default function Dashboard() {
+  return (
+    <>
+      <div className="pb-8 md:py-4 px-2 md:px-0">
+        <div>
+          <Tab.Group>
+            <div className="md:grid md:grid-cols-5 mt-4">
+              <Tab.List
+                className={
+                  "flex flex-wrap md:flex-col md:flex-nowrap gap-1 border-b-[1px] pb-4 md:border-b-0  md:col-span-1 md:border-r-[1px] md:pr-4"
+                }
+              >
+                <div className="md:flex md:flex-col gap-1">
+                  <h1 className="mb-2  md:text-lg font-semibold md:ml-3">
+                    Dashboard
+                  </h1>
+                  <Tab className={"outline-0"}>
+                    {({ selected }) => (
+                      <button
+                        className={
+                          selected
+                            ? "bg-gray-100 hover:bg-gray-200 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start"
+                            : "hover:bg-gray-100 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start"
+                        }
+                      >
+                        <HomeIcon
+                          className="h-4 w-4 mr-2 inline-block mb-1"
+                          aria-hidden="true"
+                        />
+                        Home
+                      </button>
+                    )}
+                  </Tab>
+                  <Tab className={"outline-0"}>
+                    {({ selected }) => (
+                      <button
+                        className={
+                          selected
+                            ? "bg-gray-100 hover:bg-gray-200 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start text-nowrap"
+                            : "hover:bg-gray-100 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start text-nowrap"
+                        }
+                      >
+                        <RectangleStackIcon
+                          className="h-4 w-4 mr-2 inline-block mb-1 "
+                          aria-hidden="true"
+                        />
+                        My Products
+                      </button>
+                    )}
+                  </Tab>
+                  <Tab className={"outline-0"}>
+                    {({ selected }) => (
+                      <button
+                        className={
+                          selected
+                            ? "bg-gray-100 hover:bg-gray-200 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start"
+                            : "hover:bg-gray-100 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start"
+                        }
+                      >
+                        <BanknotesIcon
+                          className="h-4 w-4 mr-2 inline-block mb-1"
+                          aria-hidden="true"
+                        />
+                        Earnings
+                      </button>
+                    )}
+                  </Tab>
+                </div>
+                <div className="md:flex md:flex-col gap-1">
+                  <h1 className="mt-2 mb-2  md:mt-6 md:text-lg font-semibold md:ml-3">
+                    Settings
+                  </h1>
+                  <Tab className={"outline-0"}>
+                    {({ selected }) => (
+                      <button
+                        className={
+                          selected
+                            ? "bg-gray-100 hover:bg-gray-200 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start"
+                            : "hover:bg-gray-100 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start"
+                        }
+                      >
+                        <UserCircleIcon
+                          className="h-4 w-4 mr-2 inline-block mb-1"
+                          aria-hidden="true"
+                        />
+                        Profile
+                      </button>
+                    )}
+                  </Tab>
+                  <Tab className={"outline-0"}>
+                    {({ selected }) => (
+                      <button
+                        className={
+                          selected
+                            ? "bg-gray-100 hover:bg-gray-200 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start"
+                            : "hover:bg-gray-100 rounded-md px-3 py-1 md:py-2 text-sm w-full text-start"
+                        }
+                      >
+                        <ShieldCheckIcon
+                          className="h-4 w-4 mr-2 inline-block mb-1"
+                          aria-hidden="true"
+                        />
+                        Account
+                      </button>
+                    )}
+                  </Tab>
+                </div>
+              </Tab.List>
+              <Tab.Panels className={"md:col-span-4 mt-4 md:mt-0 md:px-4"}>
+                <Tab.Panel>
+                  <MainPage />
+                </Tab.Panel>
+                <Tab.Panel>
+                  <div>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Repudiandae nostrum consectetur omnis corporis officiis vero
+                    perspiciatis pariatur. Commodi culpa voluptatum distinctio
+                    exercitationem fugiat corrupti recusandae laborum ut minus
+                    nulla laboriosam totam eaque necessitatibus, iure dolores
+                    nostrum alias aut quasi! Corporis nesciunt eum, quae
+                    explicabo est minima totam dolores ipsam at.
+                  </div>
+                </Tab.Panel>{" "}
+                <Tab.Panel>
+                  <div>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Repudiandae nostrum consectetur omnis corporis officiis vero
+                    perspiciatis pariatur. Commodi culpa voluptatum distinctio
+                    exercitationem fugiat corrupti recusandae laborum ut minus
+                    nulla laboriosam totam eaque necessitatibus, iure dolores
+                    nostrum alias aut quasi! Corporis nesciunt eum, quae
+                    explicabo est minima totam dolores ipsam at.
+                  </div>
+                </Tab.Panel>
+                <Tab.Panel>
+                  <div>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Repudiandae nostrum consectetur omnis corporis officiis vero
+                    perspiciatis pariatur. Commodi culpa voluptatum distinctio
+                    exercitationem fugiat corrupti recusandae laborum ut minus
+                    nulla laboriosam totam eaque necessitatibus, iure dolores
+                    nostrum alias aut quasi! Corporis nesciunt eum, quae
+                    explicabo est minima totam dolores ipsam at.
+                  </div>
+                </Tab.Panel>
+                <Tab.Panel>
+                  <div>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Repudiandae nostrum consectetur omnis corporis officiis vero
+                    perspiciatis pariatur. Commodi culpa voluptatum distinctio
+                    exercitationem fugiat corrupti recusandae laborum ut minus
+                    nulla laboriosam totam eaque necessitatibus, iure dolores
+                    nostrum alias aut quasi! Corporis nesciunt eum, quae
+                    explicabo est minima totam dolores ipsam at.
+                  </div>
+                </Tab.Panel>
+              </Tab.Panels>
+            </div>
+          </Tab.Group>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ components/dashboard/MainPage.tsx
@@ -0,0 +1,28 @@
+export default function MainPage() {
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <div>
+          <h1 className=" md:text-lg font-semibold">
+            Good Morning, Asif Ekbal 👋
+          </h1>
+          <div className="text-sm text-gray-500">
+            <p>Here's what's happening with your products today.</p>
+          </div>
+        </div>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, eius.
+          Dolorem soluta illo, rem non modi, fugiat dicta omnis delectus
+          voluptate asperiores accusamus voluptatum dignissimos ipsam. Quod nisi
+          eius laboriosam esse cupiditate aut quibusdam tenetur amet cumque,
+          repellat illum saepe culpa fuga, vitae eaque. Dolore vitae accusamus
+          dignissimos, pariatur quaerat accusantium quia quas rerum velit!
+          Perferendis reiciendis repellendus aperiam ducimus commodi laboriosam.
+          Consequuntur, tempore? Facere, cum non consequatur odit assumenda
+          fugit provident doloremque accusantium ratione hic id, dolorum alias
+          earum!
+        </div>
+      </div>
+    </>
+  );
+}
