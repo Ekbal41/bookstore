1a8354158463910618f45c93f692ded76868def3
Ekbal41 <mdasifekbalshagor@gmail.com>
2024-01-18 22:39:05 +0600

    lok

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ import { isAuthenticatedUser } from "./app/actions";
 const protectedRoutes = ["/dashboard"];
 
 export default async function middleware(req: NextRequest) {
-  // const isAuthUser = await isAuthenticatedUser();
+  const isAuthUser = await isAuthenticatedUser();
+  console.log(isAuthUser);
   // if (!isAuthUser && protectedRoutes.includes(req.nextUrl.pathname)) {
   //   const absoluteURL = new URL("/", req.nextUrl.origin);
   //   return NextResponse.redirect(absoluteURL.toString());
