27b773db2b0e6258e5cd4e20a8bd94030c1eeaca
Ekbal41 <mdasifekbalshagor@gmail.com>
2024-02-02 21:11:42 +0600

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,21 @@ import Link from "next/link";
 import StarRating from "@/components/StartRating";
 export default function Home() {
   return (
-    <div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-8">
-        {products.map((product: Product) => (
-          <div key={product.id}>
-            <ProductCard {...product} />
-          </div>
-        ))}
+    <>
+      <Hero />
+      <div>
+        <h1 className="text-xl mb-4 font-medium text-center md:text-start dark:text-gray-100">
+          New Products
+        </h1>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-8">
+          {products.map((product: Product) => (
+            <div key={product.id}>
+              <ProductCard {...product} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -45,7 +51,9 @@ const ProductCard = (product: Product) => {
                 {product.name.slice(0, 25) + "..."}
               </Link>
             </h3>
-            <p className="font-medium text-md dark:text-gray-100">৳{product.price}</p>
+            <p className="font-medium text-md dark:text-gray-100">
+              ৳{product.price}
+            </p>
           </div>
           <div className="flex justify-between items-center mt-1">
             <h3 className="font-medium text-xs dark:text-gray-100">
@@ -63,15 +71,36 @@ const ProductCard = (product: Product) => {
 const Hero = () => {
   return (
     <>
-      <div className="flex items-center justify-center my-8 ">
+      <div className="grid lg:grid-cols-2 items-center justify-between mb-10 xl:mb-20 xl:mt-16">
         <div>
-          <h1 className="font-bold text-xl lg:text-5xl md:text-3xl mb-4 text-center md:text-start">
-            Find everything you need.
-          </h1>
-          <p className="text-gray-500 text-center md:text-start">
-            <strong>The #1 Collection</strong> of Free and Premium Web Resources
-            .
-          </p>
+          <div>
+            <div className="bg-gray-100 inline-block text-sm 
+            dark:bg-gray-800 dark:text-gray-100 dark:border dark:border-gray-700
+            px-3 py-1 mb-2 rounded-full text-gray-600">
+              <p>11th Anniversary Sale!</p>
+            </div>
+            <h1 className="font-bold text-xl lg:text-5xl md:text-3xl mb-4 dark:text-gray-100">
+              Find everything you need to build your website
+            </h1>
+            <p className=" dark:text-gray-100">
+              <strong>The #1 Collection</strong> of Free and Premium Web
+              Resources.
+            </p>
+            <p className="dark:text-gray-100">
+              11th Anniversary Sale! <strong>50% off</strong> on all products.
+            </p>
+            <p className="mb-2 dark:text-gray-100">
+              20% off on all products. Use code <strong>WELCOME20</strong>
+            </p>
+          </div>
+          <div>
+            <a className="bg-blue-500 cursor-pointer hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md mt-4 inline-block">
+              View All Products
+            </a>
+          </div>
+        </div>
+        <div className=" flex justify-end items-center bg-red-10">
+         
         </div>
       </div>
     </>
